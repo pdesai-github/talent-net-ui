0068604af0bded4413188526a52dff6b52cc5eaa
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-16 16:23:23 +0530

    New working app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hwwww</h1>
+    </div>
+  );
+}
+
+export default App;
